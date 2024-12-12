121809058fa4704f2ea268c316abe7a5b08beaf9
J0yfuln'7 <153742345+J0yfuln7@users.noreply.github.com>
2024-12-12 12:42:50 +0100

    .

--- 12122024/exercices.h
+++ 12122024/exercices.h
@@ -1,19 +1,34 @@
-typedef struct Person{
-  int age;
+typedef struct Person {
+    int age;
 } Person;
+int are_persons_equals(Person *p1, Person *p2) {};
 
-typedef struct Book{
-  int pages;
+typedef struct Book {
+    int pages;
 } Book;
+int are_books_equals(Book *b1, Book *b2) {};
 
-int are_persons_equals(Person *p1, Person *p2);
-int are_books_equals(Book *p1, Book *p2);
-int are_equals(void *obj1, void *obj2, int (*compare_fnc)(void*, void*));
+int are_equals(
+    void *obj1, void *obj2,
+    int (*compare_fnc)(void *, void *)) {};
 
-int is_in_array(
-    void *obj_to_find,
+int is_greater(
+    // return 1 if obj1 is greater than obj2
+    void *obj1, void *obj2,
+    int (*compare_fnc)(void *, void *)) {};
+
+void swap(void *obj1, void *obj2, size_t size) {};
+
+void sort(
     void *array[],
-    int nb_element, // in the array
-    int elem_size,
-    int (*compare_fnc)(void*, void*)
-    );
+    int nb_of_elems_in_array,
+    size_t elem_size,
+    int (*compare_fnc)(void *, void *),
+    int (*swap_fnc)(void *, void *, int)){};
+
+int is_in_array(void *obj_to_find,
+                void *array[],
+                int nb_of_elems_in_array,
+                size_t elem_size,
+                int (*compare_fnc)(void *, void *)) {
+}
