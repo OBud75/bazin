8fbe9ddb5419aeb93eb9ea361dcf306393d71688
J0yfuln'7 <153742345+J0yfuln7@users.noreply.github.com>
2024-12-16 20:43:50 +0100

    .

--- Evaluation/cpp/user.cpp
+++ Evaluation/cpp/user.cpp
@@ -2,27 +2,22 @@
 #include "password.hpp"
 #include <iostream>
 
-// Constructor
 User::User(int id, Password password) 
     : id(id), password(std::make_unique<Password>(std::move(password))), is_logged_in(false) {}
 
-// Login function
 int User::login(const char *raw_password) {
     Password temp_password(raw_password);
     if (*password == temp_password) {
         is_logged_in = true;
-        return 1; // Success
+        return 1;
     }
-    return 0; // Failure
+    return 0;
 }
 
-// Save function
 void User::save() {
-    // Placeholder save function
-    std::cout << "User with ID " << id << " saved." << std::endl;
+    std::cout << "User avec ID " << id << " sauvegardé." << std::endl;
 }
 
-// Get user by ID
 User& User::get(int id) {
     static User user(id, Password("default"));
     return user;
